3178347e069d932c69938cc06146651f18607fc3
tinachang <gana71>
2024-02-04 17:41:32 +0800

    fix: 修正bug

--- src/numberSort.ts
+++ src/numberSort.ts
@@ -7,7 +7,6 @@
  * 輸入: [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
  * 輸出: [1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9]
  */
-
 export function sortNumbers(numbers: number[]): number[] {
   // 在此實現函式
   return numbers.sort((a, b) => a - b);
